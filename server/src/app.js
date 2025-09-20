19c741f9057b1c986c5ec5b4be410a025e4499f9
codingpratham <coding.job17@gmail.com>
2025-09-21 01:08:47 +0530

    register API

--- server/src/app.js
+++ server/src/app.js
@@ -1,11 +1,13 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 const app = express();
+import router from "./routes/index.js";
 
 // middlewares and routes
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use('/api/v1', router);
 app.get("/", (req, res) => {
   res.status(200).send("hello user");
 });
--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,10 +1,12 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-dotenv.config();
+dotenv.config({ path: "./server/.env" });
+
 
 export const connectdb = async () => {
     try {
         await mongoose.connect(process.env.DATABASE_URL);
+
         console.log("Connected to MongoDB");
     } catch (error) {
         console.log(error);
